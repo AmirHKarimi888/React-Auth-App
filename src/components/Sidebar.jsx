b92e28b96db5bf49b2a70e8185c2ee78b18b8949
Amir <amirhkarimi888@gmail.com>
2023-07-27 19:47:07 +0330

    Some changes in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,6 +13,8 @@ export const Sidebar = (props) => {
         }
     }
 
+    const signOut = props.signOut;
+
     return (
         <div className="hidden fixed sidebar items-center pt-0 ml-3 right-0 w-[100%] h-[100%] backdrop-blur-sm bg-gray-700/30" id="sidebar" onClick={ props.toggleSidebar }>
             <aside onClick={ (event) => { event.stopPropagation() } } id="logo-sidebar" className="fixed top-0 right-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x bg-white border-l border-gray-300 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
@@ -59,7 +61,7 @@ export const Sidebar = (props) => {
                 </div>
             </aside>
 
-            <CheckSignOut toggleSignOutModal={ toggleSignOutModal } signOut={ props.signOut } />
+            <CheckSignOut toggleSignOutModal={ toggleSignOutModal } signOut={ signOut } />
         </div>
     )
 }
