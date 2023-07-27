3e9f13b9b522a8f5aeda1223b4a03849638357f0
Amir <amirhkarimi888@gmail.com>
2023-07-27 17:51:12 +0330

    Main stracture of query client was changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,120 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 
+import { useEffect, useState } from "react"
+import { Action, url } from './api'
+
 import { Home } from "./pages/Home"
 import { SignUp } from './pages/SignUp'
 import { SignIn } from './pages/SignIn'
 import { NotFound } from './pages/NotFound'
 import { Header } from './components/Header'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { useQuery } from "react-query"
+
 
 function App() {
 
+  const toggleVerticalNav = () => {
+    const verticalNav = document.querySelector("#verticalNav");
+
+    if (verticalNav.classList.contains("hidden")) {
+      verticalNav.classList.remove("hidden");
+    } else {
+      verticalNav.classList.add("hidden");
+    }
+  }
+
+  const main = document.querySelector("html");
+
+  useEffect(() => {
+    const dayIcon = document.querySelector("#theme-toggle-light-icon");
+    const nightIcon = document.querySelector("#theme-toggle-dark-icon");
+
+    if (localStorage.getItem("theme")) {
+      if (localStorage.getItem("theme") == "light") {
+        dayIcon.classList.add("hidden");
+        nightIcon.classList.remove("hidden");
+
+      } else if (localStorage.getItem("theme") == "dark") {
+        main.classList.add("dark");
+        dayIcon.classList.remove("hidden");
+        nightIcon.classList.add("hidden");
+
+      }
+    }
+  }, [])
+
+  const toggleTheme = () => {
+    const dayIcon = document.querySelector("#theme-toggle-light-icon");
+    const nightIcon = document.querySelector("#theme-toggle-dark-icon");
+
+    if (main.classList.contains("dark")) {
+      main.classList.remove("dark");
+      dayIcon.classList.add("hidden");
+      nightIcon.classList.remove("hidden");
+      localStorage.setItem("theme", "light");
+
+    } else {
+      main.classList.add("dark");
+      dayIcon.classList.remove("hidden");
+      nightIcon.classList.add("hidden");
+      localStorage.setItem("theme", "dark");
+    }
+  }
+
+  const toggleSidebar = () => {
+    const sidebar = document.querySelector("#sidebar");
+
+    if (sidebar.classList.contains("hidden")) {
+      sidebar.classList.remove("hidden");
+    } else {
+      sidebar.classList.add("hidden");
+    }
+  }
+
+
+  let [users, setUsers] = useState([]);
+  let [signedUser, setSignedUser] = useState({});
+
+  useQuery(["users"], () => {
+    Action.get(url + "users", (response) => {
+      users = response.data;
+      setUsers(users);
+    })
+      .then(() => {
+        if (localStorage.getItem("signedUser")) {
+          users.filter((user) => {
+            if (parseInt(localStorage.getItem("signedUser")) === parseInt(user.userId)) {
+              signedUser = user;
+              setSignedUser(signedUser);
+            }
+          })
+        }
+      })
+  })
+
+  const signOut = () => {
+    signedUser = "";
+    setSignedUser(signedUser);
+    localStorage.setItem("signedUser", "")
+  }
+
+  useEffect(() => {
+    const toggleSidebarBtn = document.querySelector("#toggleSidebarBtn");
+    if (localStorage.getItem("signedUser") !== "") {
+      toggleSidebarBtn.classList.remove("hidden");
+      console.log(signedUser)
+    } else {
+      toggleSidebarBtn.classList.add("hidden");
+    }
+  }, [localStorage.getItem("signedUser")])
+
 
   return (
-    <div className="App">
-      <QueryClientProvider client={new QueryClient()}>
+      <div className="App">
+
         <BrowserRouter>
-          <Header />
+          <Header toggleVerticalNav={toggleVerticalNav} toggleTheme={toggleTheme} toggleSidebar={toggleSidebar} signedUser={signedUser} signOut={signOut} />
 
           <main className='my-[200px]'>
             <Routes>
@@ -28,8 +127,8 @@ function App() {
 
 
         </BrowserRouter>
-      </QueryClientProvider>
-    </div>
+
+      </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,104 +1,16 @@
-import { useEffect, useState } from "react"
 import { Sidebar } from "./Sidebar";
-import { useQuery } from "react-query";
-import { Action, url } from "../api";
 
-export const Header = () => {
+export const Header = (props) => {
 
-    const toggleVerticalNav = () => {
-        const verticalNav = document.querySelector("#verticalNav");
+    const toggleVerticalNav = props.toggleVerticalNav;
 
-        if (verticalNav.classList.contains("hidden")) {
-            verticalNav.classList.remove("hidden");
-        } else {
-            verticalNav.classList.add("hidden");
-        }
-    }
+    const toggleTheme = props.toggleTheme;
 
-    const main = document.querySelector("html");
+    const toggleSidebar = props.toggleSidebar;
+  
+    let signedUser = props.signedUser;
 
-    useEffect(() => {
-        const dayIcon = document.querySelector("#theme-toggle-light-icon");
-        const nightIcon = document.querySelector("#theme-toggle-dark-icon");
-
-        if (localStorage.getItem("theme")) {
-            if (localStorage.getItem("theme") == "light") {
-                dayIcon.classList.add("hidden");
-                nightIcon.classList.remove("hidden");
-
-            } else if (localStorage.getItem("theme") == "dark") {
-                main.classList.add("dark");
-                dayIcon.classList.remove("hidden");
-                nightIcon.classList.add("hidden");
-
-            }
-        }
-    }, [])
-
-    const toggleTheme = () => {
-        const dayIcon = document.querySelector("#theme-toggle-light-icon");
-        const nightIcon = document.querySelector("#theme-toggle-dark-icon");
-
-        if (main.classList.contains("dark")) {
-            main.classList.remove("dark");
-            dayIcon.classList.add("hidden");
-            nightIcon.classList.remove("hidden");
-            localStorage.setItem("theme", "light");
-
-        } else {
-            main.classList.add("dark");
-            dayIcon.classList.remove("hidden");
-            nightIcon.classList.add("hidden");
-            localStorage.setItem("theme", "dark");
-        }
-    }
-
-    const toggleSidebar = () => {
-        const sidebar = document.querySelector("#sidebar");
-
-        if (sidebar.classList.contains("hidden")) {
-            sidebar.classList.remove("hidden");
-        } else {
-            sidebar.classList.add("hidden");
-        }
-    }
-
-
-    let [users, setUsers] = useState([]);
-    let [signedUser, setSignedUser] = useState({});
-
-    useQuery(["users"], () => {
-        Action.get(url + "users", (response) => {
-            users = response.data;
-            setUsers(users);
-        })
-        .then(() => {
-            if(localStorage.getItem("signedUser")) {
-                users.filter((user) => {
-                    if(parseInt(localStorage.getItem("signedUser")) === parseInt(user.userId)) {
-                        signedUser = user;
-                        setSignedUser(signedUser);
-                    }
-                })
-            }
-        })
-    })
-
-    const signOut = () => {
-        signedUser = "";
-        setSignedUser(signedUser);
-        localStorage.setItem("signedUser", "")
-    }
-
-    useEffect(() => {
-        const toggleSidebarBtn = document.querySelector("#toggleSidebarBtn");
-        if(localStorage.getItem("signedUser") !== "") {
-            toggleSidebarBtn.classList.remove("hidden");
-            console.log(signedUser)
-        } else {
-            toggleSidebarBtn.classList.add("hidden");
-        }
-    }, [localStorage.getItem("signedUser")])
+    const signOut = props.signOut;
 
     return (
         <header>
