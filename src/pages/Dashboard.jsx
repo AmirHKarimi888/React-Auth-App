0b3723b795ba46e40a77ddcb8260f3210b73c7cb
Amir <amirhkarimi888@gmail.com>
2023-07-30 14:32:52 +0330

    Some tailwind errors were resolved

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,14 +5,14 @@ export const Dashboard = (props) => {
     return (
         <div className="dashboard">
             
-<div class="w-[300px] mx-auto max-w-sm bg-gray-200 border border-gray-400 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
-    <div class="flex justify-end px-4 pt-4">
+<div className="w-[300px] mx-auto max-w-sm bg-gray-200 border border-gray-400 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
+    <div className="flex justify-end px-4 pt-4">
     </div>
-    <div class="flex flex-col items-center pb-10">
-        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src={ signedUser.avatar } alt="Avatar"/>
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{ signedUser.username }</h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">{ signedUser.email }</span>
-        <span class="mt-5 text-md text-orange-500">{ signedUser.role }</span>
+    <div className="flex flex-col items-center pb-10">
+        <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={ signedUser.avatar } alt="Avatar"/>
+        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{ signedUser.username }</h5>
+        <span className="text-sm text-gray-500 dark:text-gray-400">{ signedUser.email }</span>
+        <span className="mt-5 text-md text-orange-500">{ signedUser.role }</span>
     </div>
 </div>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,15 @@ export const Home = () => {
     return (
 
         <div className="home">
-            <div class="mx-auto w-[330px] p-6 bg-gray-200 border border-gray-400 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
+            <div className="mx-auto w-[330px] p-6 bg-gray-200 border border-gray-400 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
                 <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Authentication App</h5>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Authentication App</h5>
                 </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Welcome to may app! This is an app for authentication. You can sign up to my website now!</p>
-                <a href="/signup" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Welcome to may app! This is an app for authentication. You can sign up to my website now!</p>
+                <a href="/signup" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Sign Up
-                    <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                    <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                     </svg>
                 </a>
             </div>
